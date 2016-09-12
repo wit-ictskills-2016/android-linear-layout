faea47257f4b6eb8454c9606ab2095594d3c5997
John Fitzgerald <usplitu@hotmail>
2016-09-12 20:14:32 +0100

    baseline created by Android Studio 2.1.3

---
+++ app/src/main/java/app/donation/Accept.java
@@ -0,0 +1,44 @@
+package app.donation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Accept extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_accept);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_global, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.donate:
+        startActivity(new Intent(this, Donate.class));
+        break;
+      case R.id.accept:
+        startActivity(new Intent(this, Accept.class));
+        break;
+      case R.id.reject:
+        startActivity(new Intent(this, Reject.class));
+        break;
+      case R.id.message:
+        startActivity(new Intent(this, Message.class));
+        break;
+    }
+    return true;
+  }
+
+}
